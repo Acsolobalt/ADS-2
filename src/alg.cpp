2f443fc0af6305c49ee47f235bae25a4765f8d45
Acsolobalt <78843052+Acsolobalt@users.noreply.github.com>
2021-04-22 22:17:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -31,39 +31,24 @@ return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
-int k, count = 0, left, right, midd;
-while (arr[len - 1] > value && len > 0) {
---len;
+int count = 0;
+for (int i = 0; i < len-1; i++) {
+int left = i+1, right = len;
+while (left < right) {
+int midd = (left + right)/2;
+if (arr[midd] == (value - arr[i])) {
+while (arr[midd-1] == (value - arr[i]) && (midd > left)) {
+--midd;
 }
-if (!len) {
-return 0;
-}
-for (int i = len - 1; i > 0; --i) {
-k = value - arr[i];
-left = 0;
-right = i - 1;
-midd = -1;
-while (midd != left && midd != right) {
-midd = (left + right) / 2;
-if (k < arr[midd]) {
-right = midd - 1;
-} else {
-if (k > arr[midd]) {
-left = midd + 1;
-} else {
-++count;
-left = 1;
-right = 1;
-while (arr[midd - left] == k) {
-++left;
-++count;
-}
-while ((arr[midd + right] == k) && ((midd + right) < i)) {
-++right;
+while (arr[midd] == (value - arr[i])) {
+++midd;
 ++count;
 }
 break;
-}
+} else if (arr[midd] > (value - arr[i])) {
+right = midd;
+} else {
+left = midd + 1;
 }
 }
 }
